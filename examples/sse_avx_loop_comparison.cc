89e7dcb3cd5c11034c4bd686c955583bed48bfbc
rt7 <oyasal@gmail.com>
2023-11-28 01:27:58 +0300

    avx fix

--- examples/sse_avx_loop_comparison.cc
+++ examples/sse_avx_loop_comparison.cc
@@ -22,15 +22,21 @@ namespace optkit::examples
     // Function to perform addition using AVX
     void add__avx(float *a, float *b, float *result, int size)
     {
-        __m256 *ymm_a = reinterpret_cast<__m256 *>(a);
-        __m256 *ymm_b = reinterpret_cast<__m256 *>(b);
-        __m256 *ymm_result = reinterpret_cast<__m256 *>(result);
-
-        for (int i = 0; i < size / 8; ++i)
+        // Loop for processing 8 elements at a time
+        for (int i = 0; i < size / 8; i += 8)
         {
-            ymm_result[i] = _mm256_add_ps(ymm_a[i], ymm_b[i]);
+            // Load 8 elements from Arrays A and B into AVX registers
+            __m256 avx_a = _mm256_loadu_ps(&a[i]);
+            __m256 avx_b = _mm256_loadu_ps(&b[i]);
+
+            // Perform vector addition
+            __m256 avx_result = _mm256_add_ps(avx_a, avx_b);
+
+            // Store the result back into Array C
+            _mm256_storeu_ps(&result[i], avx_result);
         }
     }
+
     // Function to perform addition in a serial manner
     void add__serial(float *a, float *b, float *result, int size)
     {
@@ -42,6 +48,7 @@ namespace optkit::examples
 
     void example__sse_avx_loop_comparison()
     {
+        const int rapl_repeat = 500000000; // 50M
         const int ARRAY_SIZE = 16;
         float aa[ARRAY_SIZE] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0};
         float bb[ARRAY_SIZE] = {16.0, 15.0, 14.0, 13.0, 12.0, 11.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0};
@@ -52,16 +59,15 @@ namespace optkit::examples
         std::cout << "========= EMPTY ===========" << std::endl;
 
         {
-            OPTKIT_RAPL_REPEAT(add_sse, Add SSE, 1000000)
+            OPTKIT_RAPL_REPEAT(add_sse, Add SSE, rapl_repeat)
             {
                 // do nothing
             }
         }
 
         std::cout << "========= SSE ===========" << std::endl;
-
         {
-            OPTKIT_RAPL_REPEAT(add_sse, Add SSE, 1000000)
+            OPTKIT_RAPL_REPEAT(add_sse, Add SSE, rapl_repeat)
             {
                 add__sse(aa, bb, result_sse, ARRAY_SIZE);
             }
@@ -70,8 +76,7 @@ namespace optkit::examples
         std::cout << "========= AVX ===========" << std::endl;
 
         {
-            OPTKIT_RAPL_REPEAT(add_avx, Add AVX, 1000000)
-
+            OPTKIT_RAPL_REPEAT(add_avx, Add AVX, rapl_repeat)
             {
                 add__avx(aa, bb, result_avx, ARRAY_SIZE);
             }
@@ -80,7 +85,7 @@ namespace optkit::examples
         std::cout << "========= SERIAL =========" << std::endl;
 
         {
-            OPTKIT_RAPL_REPEAT(add_serial, Add SERIAL, 1000000)
+            OPTKIT_RAPL_REPEAT(add_serial, Add SERIAL, rapl_repeat)
             {
                 add__serial(aa, bb, result_serial, ARRAY_SIZE);
             }
