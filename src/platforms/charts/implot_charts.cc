e5bc4d8c69a87ce021a58085c745e733bf2a4d92
rt7 <osmyasal@gmail.com>
2023-12-01 22:56:40 +0300

    remove alwaysfit

--- src/platforms/charts/implot_charts.cc
+++ src/platforms/charts/implot_charts.cc
@@ -9,25 +9,25 @@ namespace optkit::core
         ImGui::SameLine();
         ImGui::Checkbox("Horizontal", &meta.horz);
 
-        ImGui::SliderInt("group_member_count", &meta.group_member_count, 1, 3);
+        ImGui::SliderInt("Group Member Count", &meta.group_member_count, 1, 3);
         ImGui::SliderFloat("Size", &meta.size, 0, 1);
 
-        if (ImPlot::BeginPlot("##Bar Group",ImVec2{-1,-1}))
+        if (ImPlot::BeginPlot("##Bar Group", ImVec2{-1, -1}))
         {
             ImPlot::SetupLegend(ImPlotLocation_East, ImPlotLegendFlags_Outside);
             if (meta.horz)
             {
-                ImPlot::SetupAxes("Score", "Student", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
+                ImPlot::SetupAxes("Score", "Student");
                 ImPlot::SetupAxisTicks(ImAxis_Y1, meta.positions.data(), meta.glables.size(), meta.glables.data());
                 ImPlot::PlotBarGroups(meta.member_labels.data(), meta.data.data(), meta.group_member_count, meta.glables.size(), meta.size, 0, meta.flags | ImPlotBarGroupsFlags_Horizontal);
             }
             else
             {
-                ImPlot::SetupAxes("Student", "Score", ImPlotAxisFlags_AutoFit, ImPlotAxisFlags_AutoFit);
+                ImPlot::SetupAxes("Student", "Score");
                 ImPlot::SetupAxisTicks(ImAxis_X1, meta.positions.data(), meta.glables.size(), meta.glables.data());
                 ImPlot::PlotBarGroups(meta.member_labels.data(), meta.data.data(), meta.group_member_count, meta.glables.size(), meta.size, 0, meta.flags);
             }
-            
+
             ImPlot::EndPlot();
         }
     }
