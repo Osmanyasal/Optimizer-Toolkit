166be535bd4326fd06e6fef9ae0dffca68f6899b
rt7 <osmyasal@gmail.com>
2024-01-26 02:45:15 +0300

    ..

--- src/main.cc
+++ src/main.cc
@@ -7,8 +7,13 @@ int32_t main(int32_t argc, char **argv)
 {
     OptimizerKit optkit;
 
+    for (auto &info : optkit::core::Query::rapl_domain_info())
+        std::cout << info << "\n";
+
+    return 0;
+
     optkit::examples::example__sse_avx_loop_comparison();
- 
+
     std::cout << "EXECUTION DONE !!!\n";
     // const auto &val = optkit::core::rapl::from_json(read_file(EXECUTION_FOLDER_NAME + "/add_serial__rapl.json"));
     // std::cout << "FROM JSON IS DONE!\n";
