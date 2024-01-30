c2e78e7b5d2f4a53d788afca7e72ed55dac7bd4d
rt7 <oyasal@gmail.com>
2024-01-30 22:38:47 +0300

    ...

--- sandbox/beacon/beacon.cc
+++ sandbox/beacon/beacon.cc
@@ -7,11 +7,12 @@ int32_t main(int32_t argc, char **argv)
 
     int value = argc > 1 ? std::atoi(argv[1]) : -1;
     OPTKIT_INFO("Monitoring socket energy for {} sec. (-1 means indefinite)", value);
- 
+
     RaplProfiler main_block{"main_block"};
     if (value == -1)
         while (true)
         {
+            // first read's usually 0 because it executes faster than rapl update (each 1ms)
             std::cout << main_block.read().second << "\n";
             sleep(1);
         }
