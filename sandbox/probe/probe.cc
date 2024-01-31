35073d4c41e246203c8fe3752ab9f58b1271d858
rt7 <oyasal@gmail.com>
2024-01-31 16:15:36 +0300

    probeing updated

--- sandbox/probe/probe.cc
+++ sandbox/probe/probe.cc
@@ -14,14 +14,25 @@ int32_t main(int32_t argc, char **argv)
         std::cout << item << std::endl;
 
     std::cout << "\n";
-    std::cout << "============== Rapl Read Methods ==============\n";
+    std::cout << "============== Rapl Read Methods ==============\n\n";
     std::cout << "RAPL PERF read avail: [" << Query::is_rapl_perf_avail() << "]\n";
     std::cout << "RAPL powercap read avail: [" << Query::is_rapl_powercap_avail() << "]\n";
     std::cout << "RAPL MSR read avail: [" << Query::is_rapl_msr_avail() << "]\n";
 
     std::cout << "\n";
-    std::cout << "============== Avail PMUs ==============\n";
+    std::cout << "============== Default PMU ==============\n\n";
+    std::cout << Query::default_pmu_info();
+
+    std::cout << "\n";
+    std::cout << "============== Avail PMUs ==============\n\n";
     Query::list_avail_pmus();
+ 
+    // std::cout << "\n";
+    // std::cout << "============== Avail PMU Events ==============\n\n";
+    // for(int i : Query::avail_pmu_ids()){
+    //     Query::list_avail_events(i);
+    //     std::cout << "\n------------------------\n";
+    // }
 
     return 0;
 }
