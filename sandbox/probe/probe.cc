03e35b4816021aab48ec2a38a981e83012365fc0
rt7 <oyasal@gmail.com>
2024-02-05 00:49:05 +0300

    ..

--- sandbox/probe/probe.cc
+++ sandbox/probe/probe.cc
@@ -3,36 +3,41 @@
 
 int32_t main(int32_t argc, char **argv)
 {
-    OptimizerKit optkit{false};
+    optkit::utils::logger::BaseLogger::init();
+    Query::init();
 
-    std::cout << "============== Detect Packages ==============\n";
-    std::cout << Query::detect_packages() << std::endl;
-    
-    std::cout << "\n";
-    std::cout << "============== Rapl Domain Info ==============\n\n";
-    for (auto &item : Query::rapl_domain_info())
-        std::cout << item << std::endl;
+    if(argc < 2)
+    {
+        std::cout << "============== Detect Packages ==============\n";
+        std::cout << Query::detect_packages() << std::endl;
 
-    std::cout << "\n";
-    std::cout << "============== Rapl Read Methods ==============\n\n";
-    std::cout << "RAPL PERF read avail: [" << Query::is_rapl_perf_avail() << "]\n";
-    std::cout << "RAPL powercap read avail: [" << Query::is_rapl_powercap_avail() << "]\n";
-    std::cout << "RAPL MSR read avail: [" << Query::is_rapl_msr_avail() << "]\n";
+        std::cout << "\n";
+        std::cout << "============== Rapl Domain Info ==============\n\n";
+        for (auto &item : Query::rapl_domain_info())
+            std::cout << item << std::endl;
 
-    std::cout << "\n";
-    std::cout << "============== Default PMU ==============\n\n";
-    std::cout << Query::default_pmu_info();
+        std::cout << "\n";
+        std::cout << "============== Rapl Read Methods ==============\n\n";
+        std::cout << "RAPL PERF read avail: [" << Query::is_rapl_perf_avail() << "]\n";
+        std::cout << "RAPL powercap read avail: [" << Query::is_rapl_powercap_avail() << "]\n";
+        std::cout << "RAPL MSR read avail: [" << Query::is_rapl_msr_avail() << "]\n";
 
-    std::cout << "\n";
-    std::cout << "============== Avail PMUs ==============\n\n";
-    Query::list_avail_pmus();
+        std::cout << "\n";
+        std::cout << "============== Default PMU ==============\n\n";
+        std::cout << Query::default_pmu_info();
+
+        std::cout << "\n";
+        std::cout << "============== Avail PMUs ==============\n\n";
+        Query::list_avail_pmus();
+
+        // std::cout << "\n";
+        // std::cout << "============== Avail PMU Events ==============\n\n";
+        // for(int i : Query::avail_pmu_ids()){
+        //     Query::list_avail_events(i);
+        //     std::cout << "\n------------------------\n";
+        // }
+    }
  
-    // std::cout << "\n";
-    // std::cout << "============== Avail PMU Events ==============\n\n";
-    // for(int i : Query::avail_pmu_ids()){
-    //     Query::list_avail_events(i);
-    //     std::cout << "\n------------------------\n";
-    // }
-
+    Query::destroy();
     return 0;
 }
