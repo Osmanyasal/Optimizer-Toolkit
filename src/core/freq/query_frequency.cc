cafed553ec027252c51b2f5b106e156c0b4be1af
rt7 <oyasal@gmail.com>
2024-05-03 23:10:41 +0300

    ..

--- src/core/freq/query_frequency.cc
+++ src/core/freq/query_frequency.cc
@@ -5,54 +5,60 @@ namespace optkit::core::freq
 
     std::vector<long> QueryFreq::get_scaling_available_frequencies(int core)
     {
-        std::vector<long> frequencies;
-        std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_available_frequencies", false);
-        std::istringstream iss(file_content);
-        long freq;
-        while (iss >> freq)
+        static std::vector<long> frequencies;
+
+        if (OPT_UNLIKELY(frequencies.size() == 0))
         {
-            frequencies.push_back(freq);
+            std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_available_frequencies", false);
+            std::istringstream iss(file_content);
+            long freq;
+            while (iss >> freq)
+            {
+                frequencies.push_back(freq);
+            }
         }
         return frequencies;
     }
 
     long QueryFreq::get_bios_limit(int core)
     {
-        std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/bios_limit", false);
+        static std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/bios_limit", false);
         return std::stol(file_content);
     }
 
     std::string QueryFreq::get_scaling_driver(int core)
     {
-        return read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_driver", false);
+        static std::string scaling_driver = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_driver", false);
+        return scaling_driver;
     }
 
     std::string QueryFreq::get_scaling_governor(int core)
     {
-        return read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_governor", false);
+        static std::string scaling_governor = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_governor", false);
+        return scaling_governor;
     }
 
     long QueryFreq::get_scaling_max_limit(int core)
     {
-        std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_max_freq", false);
+        static std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_max_freq", false);
         return std::stol(file_content);
     }
 
     long QueryFreq::get_scaling_min_limit(int core)
     {
-        std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_min_freq", false);
+        static std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/scaling_min_freq", false);
         return std::stol(file_content);
     }
 
-    long QueryFreq::get_cpuinfo_scaling_max_limit(int core)
+    long QueryFreq::get_cpuinfo_max_freq(int core)
     {
-        std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/cpuinfo_max_freq", false);
+        static std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/cpuinfo_max_freq", false);
         return std::stol(file_content);
     }
 
-    long QueryFreq::get_cpuinfo_scaling_min_limit(int core)
+    long QueryFreq::get_cpuinfo_min_freq(int core)
     {
-        std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/cpuinfo_min_freq", false);
+        static std::string file_content = read_file("/sys/devices/system/cpu/cpu" + std::to_string(core) + "/cpufreq/cpuinfo_min_freq", false);
         return std::stol(file_content);
     }
 }
--- src/core/freq/query_frequency.hh
+++ src/core/freq/query_frequency.hh
@@ -18,8 +18,8 @@ namespace optkit::core::freq
         static long get_scaling_max_limit(int core = 0);
         static long get_scaling_min_limit(int core = 0);
     
-        static long get_cpuinfo_scaling_max_limit(int core = 0);
-        static long get_cpuinfo_scaling_min_limit(int core = 0);
+        static long get_cpuinfo_max_freq(int core = 0);
+        static long get_cpuinfo_min_freq(int core = 0);
     
     private:
         QueryFreq();
