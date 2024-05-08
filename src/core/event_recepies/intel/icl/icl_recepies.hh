fbe7f25d1063db45835720eee0af4e0e0eefb030
rt7 <osmyasal@gmail.com>
2024-05-09 00:39:07 +0300

    ..

--- src/core/event_recepies/intel/icl/icl_recepies.hh
+++ src/core/event_recepies/intel/icl/icl_recepies.hh
@@ -12,10 +12,27 @@ namespace optkit::core::recepies::intel::icl
         ComputationalIntensity() {}
         virtual ~ComputationalIntensity() {}
 
+        // virtual const std::vector<std::pair<uint64_t, std::string>> get_recepies() override
+        // {
+        //     return {{optkit::intel::icl::INSTRUCTIONS_RETIRED, "instructions_retired"},
+        //             {optkit::intel::icl::MEM_LOAD_RETIRED | optkit::intel::icl::MEM_LOAD_RETIRED__MASK__INTEL_ICL_MEM_LOAD_RETIRED__L3_MISS, "l3_cache_miss"}};
+        // }
+
         virtual const std::vector<std::pair<uint64_t, std::string>> get_recepies() override
         {
-            return {{optkit::intel::icl::INSTRUCTIONS_RETIRED, "instructions_retired"}, 
-            {optkit::intel::icl::MEM_LOAD_RETIRED | optkit::intel::icl::MEM_LOAD_RETIRED__MASK__INTEL_ICL_MEM_LOAD_RETIRED__L3_MISS, "l3_cache_miss"}};
+            return {{optkit::intel::icl::FP_ARITH_INST_RETIRED |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__SCALAR |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__SCALAR_SINGLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__SCALAR_DOUBLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__VECTOR |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__128B_PACKED_DOUBLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__128B_PACKED_SINGLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__256B_PACKED_DOUBLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__256B_PACKED_SINGLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__512B_PACKED_DOUBLE |
+                         optkit::intel::icl::FP_ARITH__MASK__INTEL_ICL_FP_ARITH_INST_RETIRED__512B_PACKED_SINGLE,
+                     "fp_arith"},
+                    {optkit::intel::icl::MEM_LOAD_RETIRED | optkit::intel::icl::MEM_LOAD_RETIRED__MASK__INTEL_ICL_MEM_LOAD_RETIRED__L3_MISS, "l3_cache_miss"}};
         }
     };
 }
--- src/main.cc
+++ src/main.cc
@@ -22,15 +22,44 @@ int32_t main(int32_t argc, char **argv)
     // CPUFrequency::reset_core_frequency(0);
     // CPUFrequency::reset_uncore_frequency(0);
 
-    // float aa = 0;
-    {
-        optkit::core::recepies::intel::icl::ComputationalIntensity ci;
-        OPTKIT_PERFORMANCE_EVENTS("temp block", "Computational Intensity", pp, ci.get_recepies());
+    optkit::core::recepies::intel::icl::ComputationalIntensity ci;
 
-        // for (int i = 0; i < 100; i++)
-        //     aa += i;
+    {
+        double aa = 0;
+        OPTKIT_PERFORMANCE_BLOCK_EVENTS("for_loop", "Computational Intensity", pp, ci.get_recepies());
+
+        for (int i = 0; i < 1000000; i++)
+            aa =  aa + i * 0.052;
+
+        std::cout << aa << "\n"; 
+    }
+
+    {
+        OPTKIT_PERFORMANCE_EVENTS("file_read", "Computational Intensity", pp, ci.get_recepies());
 
         std::cout << QueryFreq::get_bios_limit();
     }
+
+    {
+        OPTKIT_PERFORMANCE_EVENTS("multiple_file_read", "Computational Intensity", pp, ci.get_recepies());
+
+        std::cout << QueryFreq::get_bios_limit() << QueryFreq::get_cpuinfo_max_freq() << QueryFreq::get_cpuinfo_min_freq() << QueryFreq::get_scaling_driver() << QueryFreq::get_scaling_max_limit();
+    }
+
+    {
+        OPTKIT_PERFORMANCE_EVENTS("multiple_file_write", "Computational Intensity", pp, ci.get_recepies());
+
+        for (int i = 0; i < 100; i++)
+        {
+            CPUFrequency::set_core_frequency(1200000, 0);
+
+            CPUFrequency::set_core_frequency(1300000, 0);
+
+            CPUFrequency::set_core_frequency(3300000, 0);
+        }
+    }
+
+    CPUFrequency::reset_core_frequency(0);
+
     return 0;
 }
