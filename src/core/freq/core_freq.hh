88ab3dc8f157c35c469fde2b89742e861f6a7375
rt7 <osmyasal@gmail.com>
2024-05-12 12:29:55 +0300

    set_cpu_freq macro is detailed

--- src/core/freq/core_freq.hh
+++ src/core/freq/core_freq.hh
@@ -13,9 +13,9 @@ using optkit::core::freq::QueryFreq;
 
 #define OPTKIT_SET_CPU_CORE_FREQ(frequency, socket) CPUFrequency::set_core_frequency(frequency, socket)
 #define OPTKIT_SET_CPU_UNCORE_FREQ(frequency, socket) CPUFrequency::set_uncore_frequency(frequency, socket)
-#define OPTKIT_SET_CPU_FREQ(frequency, socket)     \
-    OPTKIT_SET_CPU_UNCORE_FREQ(frequency, socket); \
-    OPTKIT_SET_CPU_CORE_FREQ(frequency, socket)
+#define OPTKIT_SET_CPU_FREQ(core_freq,uncore_freq, socket)     \
+    OPTKIT_SET_CPU_UNCORE_FREQ(core_freq, socket); \
+    OPTKIT_SET_CPU_CORE_FREQ(uncore_freq, socket)
 
 #define OPTKIT_RESET_CPU_CORE_FREQ(socket) CPUFrequency::reset_core_frequency(socket)
 #define OPTKIT_RESET_CPU_UNCORE_FREQ(socket) CPUFrequency::reset_uncore_frequency(socket)
