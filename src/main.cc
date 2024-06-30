dbee36adc0dde4b4cde3f8ac02a18b82117f3057
rt7 <oyasal@gmail.com>
2024-06-30 20:35:13 +0300

    ..

--- src/main.cc
+++ src/main.cc
@@ -21,37 +21,37 @@ int32_t main(int32_t argc, char **argv)
         std::cout << "Uncore current: " << CPUFrequency::get_uncore_frequency(i) << "\n";
     }
  
-    return 0;
-    
-    // freq_governors::intel::icl::Governor gg;
-    // BLOCK_TIMER("Whole Program");
-    // {
-
-    //     BLOCK_TIMER("Operation Block");
-    //     double aa = 0;
-
-    //     #pragma omp parallel for
-    //     for (int32_t i = 0; i < 20000000; i++) 
-    //         aa = aa + i * 0.052; // 2 * 50M -> 100M
-
-    //     std::cout << aa << std::endl; 
-    // }
-
-    // {
-    //     // freq_governors::intel::icl::Governor gg; 
-    //     BLOCK_TIMER("IO Block");
-
-    //     #pragma omp parallel for
-    //     for (int32_t i = 0; i < 1000; i++)
-    //     {
-    //         QueryFreq::get_bios_limit();
-    //         QueryFreq::get_cpuinfo_max_freq();
-    //         QueryFreq::get_cpuinfo_min_freq();
-    //         QueryFreq::get_scaling_driver();
-    //         QueryFreq::get_scaling_governor();
-    //         QueryFreq::get_scaling_max_limit();
-    //         QueryFreq::get_scaling_min_limit();
-    //     }
-    // }
     // return 0;
+    
+    freq_governors::intel::skl::Governor gg;
+    BLOCK_TIMER("Whole Program");
+    {
+
+        BLOCK_TIMER("Operation Block");
+        double aa = 0;
+
+        #pragma omp parallel for
+        for (int32_t i = 0; i < 20000000; i++) 
+            aa = aa + i * 0.052; // 2 * 50M -> 100M
+
+        std::cout << aa << std::endl; 
+    }
+
+    {
+        // freq_governors::intel::icl::Governor gg; 
+        BLOCK_TIMER("IO Block");
+
+        #pragma omp parallel for
+        for (int32_t i = 0; i < 1000; i++)
+        {
+            QueryFreq::get_bios_limit();
+            QueryFreq::get_cpuinfo_max_freq();
+            QueryFreq::get_cpuinfo_min_freq();
+            QueryFreq::get_scaling_driver();
+            QueryFreq::get_scaling_governor();
+            QueryFreq::get_scaling_max_limit();
+            QueryFreq::get_scaling_min_limit();
+        }
+    }
+    return 0;
 }
