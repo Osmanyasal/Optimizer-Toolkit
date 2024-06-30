d8ea8ceab1c6e975ca21add9b65ea9faa26be70b
rt7 <oyasal@gmail.com>
2024-06-30 21:04:14 +0300

    ..

--- src/core/freq/base_governor.cc
+++ src/core/freq/base_governor.cc
@@ -9,7 +9,7 @@ namespace optkit::core::freq
     BaseGovernor::BaseGovernor(int64_t sample_period) : config{false, true, false, 0, -1}, sample_period{sample_period}
     {
         this->config.perf_event_config.sample_period = this->sample_period;
-        this->config.cpu = 1; /// set thread 1 for cpu_cycle callbacks!!
+        this->config.cpu = 1; /// set thread 1 for cpu_cycle callbacks!!    // TODO: This must be changed based on the cores given to the program!
 
         memset(&sa, 0, sizeof(struct sigaction));
         sa.sa_sigaction = BaseGovernor::call_back;
@@ -34,7 +34,7 @@ namespace optkit::core::freq
         current_governor->disalbe_callback_trigger();
 
         static int32_t i = 0;
-        OPTKIT_CORE_INFO("{} th call_back() called", ++i);
+        // OPTKIT_CORE_INFO("{} th call_back() called", ++i);
         current_governor->snapshot_pmus();
         double compute_intensity = current_governor->compute_intensity();
         double io_intensity = current_governor->io_intensity();
