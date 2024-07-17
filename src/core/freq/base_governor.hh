786014995ce83b9771763ba7a91a98d69dd1700d
rt7 <oyasal@gmail.com>
2024-07-17 03:14:18 +0300

    ..

--- src/core/freq/base_governor.hh
+++ src/core/freq/base_governor.hh
@@ -15,7 +15,7 @@
 
 
 #define OPTKIT_BASE_GOVERNOR_MMAP_PAGES 8
-#define OPTKIT_BASE_GOVERNOR_GOVERNOR_CALLBACK_PERIOD_MS 3000 // 3 call per 1sec
+#define OPTKIT_BASE_GOVERNOR_GOVERNOR_CALLBACK_PERIOD_MS 1000 // 3 call per 1sec
 #define GHZ 1000000000
 namespace optkit::core::freq
 {
